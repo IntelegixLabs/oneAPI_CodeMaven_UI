d8c66112705d826ff9c7c06e83f313f254d5f374
sayan <sayansinha5@gmail.com>
2023-04-06 23:21:31 +0530

    :sparkles: add footer

--- src/components/common/layout/GuestLayout.jsx
+++ src/components/common/layout/GuestLayout.jsx
@@ -3,6 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 import { useStateContext } from "../../../context/ContextProvider.jsx";
 import Navbar from "../../guest/Navbar.jsx";
+import Footer from "../../guest/Footer.jsx";
 
 export default function GuestLayout() {
   const { token } = useStateContext();
@@ -15,6 +16,7 @@ export default function GuestLayout() {
     <React.Fragment>
       <Navbar />
       <Outlet />
+      <Footer />
     </React.Fragment>
   );
 }
