bc11220b64f19aec49a29200ce0c32554518525b
sayan <sayansinha5@gmail.com>
2023-04-07 00:03:32 +0530

    :lipstick: update app contents

--- src/components/guest/AboutComponent.jsx
+++ src/components/guest/AboutComponent.jsx
@@ -1,9 +1,25 @@
 export default function AboutComponent() {
   return (
-    <div className="bg-faint-blue">
-      <h1 className="font-bold p-5">
-        About
-      </h1>
+    <div className="mt-4 mb-5 mt-sm-5 container">
+      <div className="row mx-2 mx-sm-0">
+        <div className="col-md-6">
+          <h1 className="mb-4 font-bold">About</h1>
+          <p>
+            Machine Learning has been revolutionizing on healthcare domain. ML
+            models can now detect patterns that depicts an underlying diseases.
+          </p>
+          <p>
+            In this way, the AI techniques can be considered as second pair of
+            eyes that can decode patient health knowledge extracted from large
+            data sets by summing up facts & observations of diseases.
+          </p>
+          <p>
+            Due to COVID-19 pandemic, existing digital diagnosis methods are
+            being more preferred as people are willing to follow Covid norms &
+            stay safe.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
