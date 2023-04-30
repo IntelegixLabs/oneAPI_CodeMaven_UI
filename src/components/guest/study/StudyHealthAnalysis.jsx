461e710f6f4b20970c59b3cfa1b0274f53ecf899
subhransu das <subhranshu.das@bjshomedelivery.com>
2023-04-30 19:20:37 +0530

    study Breast cancer information added

--- src/components/guest/study/StudyHealthAnalysis.jsx
+++ src/components/guest/study/StudyHealthAnalysis.jsx
@@ -1,11 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link,useLocation } from "react-router-dom";
+import CardLink from "../../common/card/CardLink.jsx";
 
 export default function StudyHealthAnalysis() {
+  const currentRoute = useLocation().pathname;
+  const StudyhealthAnalysisCategory = [
+    {
+      title: "Breast Cancer Prediction",
+      path: "/breast-cancer",
+    },
+  ];
+  const displayStudyHealthAnalysisCategories = () => {
+    let studyhealthAnalysisCategories = StudyhealthAnalysisCategory.map((category, index) => {
+      return (
+        <div className="col-12 col-sm-6 col-lg-4" key={index}>
+          <CardLink
+            to={currentRoute + category.path}
+            title={category.title}
+          />
+        </div>
+      );
+    });
+
+    if (StudyhealthAnalysisCategory.length > 0) {
+      return (
+        <div className="row animated fadeInDown g-4">
+          {studyhealthAnalysisCategories}
+        </div>
+      );
+    } else {
+      return <div className="text-center">No categories found</div>;
+    }
+  };
+
   return (
     <React.Fragment>
       <h1>Study Health Analysis</h1>
       <Link to="/study/health-analysis/breast-cancer">Breast Cancer</Link>
+      {displayStudyHealthAnalysisCategories()}
     </React.Fragment>
   );
 }
--- src/components/guest/study/health-analysis/StudyBreastCancer.jsx
+++ src/components/guest/study/health-analysis/StudyBreastCancer.jsx
@@ -1,5 +1,36 @@
+import FemaleBreastDiagram from "../../../../assets/images/female-breast-diagram-750px.jpg";
+import { Fragment } from "react";
 export default function StudyBreastCancer() {
   return (
-    <h1>Study Breast Cancer</h1>
+    <Fragment>
+      <h1>Study Breast Cancer</h1>
+      <div className="row justify-content-center align-content-md-center">
+        <div className="col-md-6">
+          <p>Breast cancer is a disease in which cells in the breast grow out of control.
+            There are different kinds of breast cancer. The kind of breast cancer depends on which cells in the breast turn into cancer.</p>
+          <p>
+            Breast cancer can begin in different parts of the breast.
+            <br />A breast is made up of three main parts:
+            <ul>
+              <li>lobules</li>
+              <li>ducts</li>
+              <li>connective tissue</li>
+            </ul>
+            The lobules are the glands that produce milk. The ducts are tubes that carry milk to the nipple.
+            The connective tissue (which consists of fibrous and fatty tissue) surrounds and holds everything together.
+            Most breast cancers begin in the ducts or lobules.
+            Breast cancer can spread outside the breast through blood vessels and lymph vessels. When breast cancer spreads to other parts of the body, it is said to have metastasized.
+          </p>
+        </div>
+        <div>
+          You can predict breast cancer from <a href="#">predict breast cancer</a>
+          <p>There are few things which you need to have value for predict breast cencer</p>
+          
+        </div>
+        <div className="col-md-2">
+          <img className="img-fluid" src={FemaleBreastDiagram} />
+        </div>
+      </div>
+    </Fragment>
   )
 }
