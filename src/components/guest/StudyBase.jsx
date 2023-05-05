28ecbe6176f5562a6bf9c081bf5cc0cebc16ffc5
sayan <sayansinha5@gmail.com>
2023-05-05 21:55:59 +0530

    :lipstick: :recycle: refactor UI StudyBreastCancer and StudyDiabetic in StudyBase

--- src/components/guest/StudyBase.jsx
+++ src/components/guest/StudyBase.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import CardLink from "../common/card/CardLink";
 
 export default function Study() {
   return (
@@ -7,13 +7,30 @@ export default function Study() {
       <h1 className="mb-5 font-bold">
         Study <span className="text-theme-red">Base</span>
       </h1>
-      <div className="row mx-2 mx-sm-0">
+      <div className="row animated fadeInDown">
         <div className="col-12 col-sm-6">
-          <Link to="/study/health-analysis" className="row">
-            <div className="col-md-8">
-              <h4>Disease Dictionary</h4>
-            </div>
-          </Link>
+          <CardLink
+            to="/study/health-analysis"
+            title="DISEASE DICTIONARY"
+            description="Learn about diseases, its existence, how it affects us and what are these values represent."
+            btnText="Read Out"
+          >
+            <h1 className="mb-4">
+              <i className="fa-solid fa-book fa-lg fa-fw"></i>
+            </h1>
+          </CardLink>
+        </div>
+        <div className="col-12 col-sm-6">
+          <CardLink
+            to="/study/precautions"
+            title="PRECAUTIONS"
+            description="Learn the healthy ways and how to treat your diseases, stay healthy and keep your body under control."
+            btnText="Read Out"
+          >
+            <h1 className="mb-4">
+              <i class="fa-solid fa-stethoscope fa-fw"></i>
+            </h1>
+          </CardLink>
         </div>
       </div>
     </div>
